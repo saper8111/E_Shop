36a73370b6ebdcdf0cf955674250b19bffaebf3f
saper8111 <saper.8111@gmail.com>
2020-01-28 20:41:55 +0300

    Удалены ненужные (пока) методы

--- e-shop-web-tests/e-shop-web-tests/CouponCreationTest.cs
+++ e-shop-web-tests/e-shop-web-tests/CouponCreationTest.cs
@@ -69,52 +69,5 @@ namespace WebEshopTests
             driver.FindElement(By.XPath("//a[2]/span")).Click();
             driver.FindElement(By.XPath("//li[6]/a/i")).Click();
         }
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-
-        private string CloseAlertAndGetItsText()
-        {
-            try
-            {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert)
-                {
-                    alert.Accept();
-                }
-                else
-                {
-                    alert.Dismiss();
-                }
-                return alertText;
-            }
-            finally
-            {
-                acceptNextAlert = true;
-            }
-        }
     }
 }
